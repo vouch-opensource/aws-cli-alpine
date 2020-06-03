7f182a2f4bece1fa0f44e185e420468bddabeb6f
Stijn Opheide <stijn.opheide@vouch.io>
2020-06-03 10:59:08 +0200

    build without cache to ensure AWS CLI gets updated

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 
 IMAGE=vouchio/aws-cli
-docker build -t $IMAGE .
-VERSION=$(docker run -it vouch.io/aws-cli aws --version | awk '{ print $1 }' | sed 's|aws-cli/||')
+docker build --no-cache -t $IMAGE .
+VERSION=$(docker run -it $IMAGE aws --version | awk '{ print $1 }' | sed 's|aws-cli/||')
 
 docker tag $IMAGE $IMAGE:$VERSION
 docker tag $IMAGE $IMAGE:latest
 
 docker push $IMAGE:$VERSION
 docker push $IMAGE:latest
+
+echo "Build $IMAGE:$VERSION"
